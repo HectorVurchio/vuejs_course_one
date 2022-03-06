7682dd7122ced2d5b4994058b7ce45d5ef92861c
Hector Vurchio <hectorvurchio@gmail.com>
2022-03-06 08:27:36 -0400

    Error Handling and 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Oops!</h1>
+  <h3>The page {{ resource }} you're llokingfor is not here.</h3>
+  <router-link :to="{ name: 'EventList' }">Back to the home page</router-link>
+</template>
+<script>
+export default {
+  resource: {
+    type: String,
+    required: true,
+    default: 'page',
+  },
+}
+</script>
